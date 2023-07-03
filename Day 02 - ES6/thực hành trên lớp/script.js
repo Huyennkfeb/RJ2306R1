0c27bf962872706a881aaea821ecb2d8f59cf03a
Huyennkfeb <huyennk.feb@gmail.com>
2023-07-03 16:46:59 +0700

    bai tap su dung map

---
+++ Day 02 - ES6/thực hành trên lớp/script.js
@@ -0,0 +1,22 @@
+
+// const a = [4,5,6,7]
+
+//Map
+// const result = a.map (item => {
+//     return item + 1
+// });
+// console.log(result);
+
+//filter
+// const result = a.filter (item => {
+//     return item % 2 == 0;
+// });
+// console.log(result);
+// const result = a.filter ( item => {
+//     return item % 2 !== 0 ;
+// });
+// console.log(result);
+
+//set
+const animal = new Set("dog,cat,bird,bear");
+console.log(animal);
