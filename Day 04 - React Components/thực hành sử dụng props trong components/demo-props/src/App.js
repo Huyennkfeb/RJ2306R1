25990673690baeeb89f74cc90430d6e0aeb697d1
Huyennkfeb <huyennk.feb@gmail.com>
2023-07-07 18:18:54 +0700

    thuc hanh xay dung function component

--- Day 04 - React Components/thực hành sử dụng props trong components/demo-props/src/App.js
+++ Day 04 - React Components/thực hành sử dụng props trong components/demo-props/src/App.js
@@ -23,12 +23,21 @@
 // }
 
 // export default App;
-import Welcome from './components/Welcome';
+// import Welcome from './components/Welcome';
+// function App() {
+//     return (
+//         <div className="App">
+//             <Welcome name="Admin" />
+//         </div>
+//     );
+// }
+// export default App;
+
+
+import AddComponent from './components/AddComponent';
 function App() {
     return (
-        <div className="App">
-            <Welcome name="Admin" />
-        </div>
+        <AddComponent firstNumber={1} secondNumber={2} />
     );
 }
 export default App;
